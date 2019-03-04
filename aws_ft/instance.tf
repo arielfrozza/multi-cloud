ff9014efa44d4f6fabddefe21a37afa420339092
Ariel Frozza <arielfrozza@gmail.com>
2019-03-03 22:21:33 -0300

    criado sshkey e complementado instance.tf e vars.tf e criado simple_app.py

--- aws_ft/instance.tf
+++ aws_ft/instance.tf
@@ -1,4 +1,26 @@
-resource "aws_instance" "mv01" {
+resource "aws_key_pair" "mykey" {
+  key_name = "mykey"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+resource "aws_instance" "vm01" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.mykey.key_name}"
+
+  provisioner "file" {
+    source = "simple_app.py"
+    destination = "/opt/simple_app.py"
+  }
+  connection {
+    user = " ${var.INSTANCE_USERNAME}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get -y install python python-pip python-setuptools python-dev build-essential",
+      "sudo pip install flask",
+      "FLASK_APP=/opt/simple_app.py nohup flask run --host=0.0.0.0 &"
+    ]
+  }
 }
